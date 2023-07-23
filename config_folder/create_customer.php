10867c34c6dff1700b302a8990ad322ae0621180
Lau Chewvy <chewvy213069@dasein.edu.my>
2023-07-23 21:11:27 +0800

    done customer.php

--- config_folder/create_customer.php
+++ config_folder/create_customer.php
@@ -2,6 +2,14 @@
 <html>
 
 <head>
+
+    <title>PDO - Create a Record - PHP CRUD Tutorial</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!-- Latest compiled and minified Bootstrap CSS (Apply your
+Bootstrap here -->
     <style>
         .error-message {
             color: red;
@@ -177,6 +185,9 @@
             </table>
         </form>
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
 </body>
 
 </html>
