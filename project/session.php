48521b67c9b5bfd0494b21acfa396c68f57cc569
Lau Chewvy <chewvy213069@dasein.edu.my>
2023-08-13 16:49:01 +0800

    created session

---
+++ project/session.php
@@ -0,0 +1,8 @@
+<?php
+
+// Check if the user is not logged in and not on the login page
+if (!isset($_SESSION['username']) && basename($_SERVER['PHP_SELF']) !== 'User_login.php') {
+    header("Location: User_login.php");
+    exit();
+}
+?>
