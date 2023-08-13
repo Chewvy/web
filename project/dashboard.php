fcaccc95b52f2fc631b6cb43f43510d5ceb11b4f
Lau Chewvy <chewvy213069@dasein.edu.my>
2023-08-13 16:49:37 +0800

    added anvbar

--- project/dashboard.php
+++ project/dashboard.php
@@ -9,16 +9,22 @@
         integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
 </head>
 
+<?php
+include 'navbar.php';
+?>
+
 <body>
+
     <!-- container -->
     <div class="container">
-        <div class="page-header">
-            <h1>Dashboard</h1>
-        </div>
-
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
             integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
             crossorigin="anonymous"></script>
 </body>
+
+<?php
+include 'config_folder/database.php';
+
+?>
 
 </html>
