a3826b3cd8126e4ea459c349064f9eab808a3293
Lau Chewvy <chewvy213069@dasein.edu.my>
2023-08-14 14:27:48 +0800

    added create customer file into the navbar

--- project/navbar.php
+++ project/navbar.php
@@ -29,6 +29,10 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="create_customer.php">Create
+                                Customer</a>
+                        </li>
+                        <li class="nav-item">
                             <a class="nav-link active" aria-current="page" href="create_product.php">Create Product</a>
                         </li>
                         <li class="nav-item">
