78269d079926cddda6c7b3a2c03dbb953a5eb40d
Jerrie <chewvy213069@dasein.edu.my>
2023-09-02 23:05:53 +0800

    changedd ID , removed password, account status

--- project/customer_index.php
+++ project/customer_index.php
@@ -1,62 +1,62 @@
 <?php
 session_start();
 ?>
+
 <!DOCTYPE HTML>
 <html>
 
 <head>
-    <title>PDO - Read Customers - PHP CRUD Tutorial</title>
-    <!-- Latest compiled and minified Bootstrap CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <title>PDO - Read Records - PHP CRUD Tutorial</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <!-- Latest compiled and minified Bootstrap CSS (Apply your Bootstrap here -->
 </head>
 
-<?php
-include 'navbar.php';
-?>
-
 <body>
-    <!-- container -->
+    <?php
+    include 'navbar.php';
+    ?>
+
     <div class="container">
         <div class="page-header">
             <h1>Read Customers</h1>
         </div>
 
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get" class="d-flex">
-            <?php echo "<a href='create_customer.php' class='btn btn-primary m-b-1em'>Create New Customer</a>"; ?>
-            <input type='search' name="search" class="ml-auto"> <!-- Use ml-auto to push to the right -->
-            <input type='submit' value='Search' class='btn btn-primary m-r-1em'>
+        <div class="d-flex justify-content-between mb-4">
+            <a href="create_customer.php" class="btn btn-primary m-b-1em">Create New Customer</a>
 
-        </form>
+            <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="get" class="d-flex">
+                <input type="search" id="search" name="search" class="form-control me-2" />
+                <input type="submit" value="Search" class="btn btn-warning" />
+            </form>
+        </div>
 
-        </form>
 
         <?php
         // include database connection
         include 'config_folder/database.php';
 
-        $query = "SELECT customer_id, username, email,password, first_name, last_name, gender, DOB, account_status,registration_date_and_time FROM customer ORDER BY username ASC";
+        $query = "SELECT customer_id, username, email, first_name, last_name, gender, DOB, registration_date_and_time, account_status FROM customer
+            ORDER BY username ASC";
 
-        // delete message prompt will be here
-        
-        // select all data
         if ($_GET) {
             $search = $_GET['search'];
 
-            if (empty($search)) { //才知道里面是不是空的，所以才在这里check
-                echo "<div class='alert alert-danger'>Search by Keyword</div>";
+            if (empty($search)) {
+                echo "<div class='alert alert-danger'>Please enter a product keyword</div>";
             }
 
-            $query = "SELECT customer_id, username,email, password, first_name, last_name, gender, DOB, account_status,registration_date_and_time  FROM customer WHERE 
-            customer_id LIKE '%$search%';
-            username LIKE '%$search%';
-            first_name LIKE '%$search%';
-            last_name LIKE '%$search%';
-            email LIKE '%$search%';
+            $query = "SELECT customer_id, username, email, first_name, last_name, gender, DOB, registration_date_and_time, account_status FROM customer WHERE 
+            username LIKE '%$search%' OR
+            first_name LIKE '%$search%' OR
+            last_name LIKE '%$search%'
             ORDER BY username ASC";
         }
+
+        // delete message prompt will be here
+
+        // select all data
 
         $stmt = $con->prepare($query);
         $stmt->execute();
@@ -64,67 +64,77 @@ include 'navbar.php';
         // this is how to get number of rows returned
         $num = $stmt->rowCount();
 
-        // link to create record form
-        
 
-        // check if more than 0 record found
+        //check if more than 0 record found
         if ($num > 0) {
 
-            // data from database will be here
-            echo "<table class='table table-hover table-responsive table-bordered'>";
-            // creating our table heading
+            // data from database will be here >
+
+            echo "<table class='table table-hover table-responsive table-bordered'>"; //start table
+
+            //creating our table heading
             echo "<tr>";
-            echo "<th>Customer ID</th>";
+            echo "<th>ID</th>";
             echo "<th>Username</th>";
             echo "<th>Email</th>";
-            echo "<th>Password</th>";
             echo "<th>First Name</th>";
             echo "<th>Last Name</th>";
             echo "<th>Gender</th>";
             echo "<th>Date of Birth</th>";
-            echo "<th>Account Status</th>";
             echo "<th>Registration Date and Time</th>";
-            echo "<th>Actions</th>";
+            echo "<th>Account Status</th>";
+            echo "<th>Action</th>";
             echo "</tr>";
+
+
+            // table body will be here
 
             // retrieve our table contents
             while ($row = $stmt->fetch(PDO::FETCH_ASSOC)) {
                 // extract row
+                // this will make $row['firstname'] to just $firstname only
                 extract($row);
                 // creating new table row per record
                 echo "<tr>";
                 echo "<td>{$customer_id}</td>";
                 echo "<td>{$username}</td>";
                 echo "<td>{$email}</td>";
-                echo "<td>{$password}</td>";
                 echo "<td>{$first_name}</td>";
                 echo "<td>{$last_name}</td>";
                 echo "<td>{$gender}</td>";
                 echo "<td>{$DOB}</td>";
-                echo "<td>{$account_status}</td>";
                 echo "<td>{$registration_date_and_time}</td>";
+                echo "<td>{$account_status}</td>";
                 echo "<td>";
                 // read one record
-                echo "<a href='customer_read_one.php?id={$username}' class='btn btn-info m-r-1em'>Read</a>";
+                echo "<a href='customer_read_one.php?customer_id={$customer_id}' class='btn btn-info' style='margin-right: 1em;'>Read</a>";
 
-                // we will use this links on the next part of this post
-                echo "<a href='customer_update.php?customer_id={$customer_id}' class='btn btn-primary m-r-1em'>Edit</a>";
+                // we will use this links on next part of this post
+                echo "<a href='customer_update.php?customer_id={$customer_id}' class='btn btn-primary' style='margin-right: 1em;'>Edit</a>";
 
-                // we will use this links on the next part of this post
-                echo "<a href='#' onclick='delete_user(\"{$username}\");' class='btn btn-danger'>Delete</a>";
+                // we will use this links on next part of this post
+                echo "<a href='#' onclick='delete_user({$username});'  class='btn btn-danger'>Delete</a>";
                 echo "</td>";
                 echo "</tr>";
             }
 
             // end table
             echo "</table>";
-        } else {
-            // if no records found
+        }
+        // if no records found
+        else {
             echo "<div class='alert alert-danger'>No records found.</div>";
         }
         ?>
 
+
+
     </div> <!-- end .container -->
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+
+    <!-- confirm delete record will be here -->
 
 </body>
 
