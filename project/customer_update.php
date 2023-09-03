9ec4f0fe1e1db440bcbe6d36b6276a9cdc4433a9
Jerrie <chewvy213069@dasein.edu.my>
2023-09-03 21:33:38 +0800

    done update image

--- project/customer_update.php
+++ project/customer_update.php
@@ -44,25 +44,21 @@
 
         // Read the current record's data
         try {
-            $query = "SELECT customer_id, username, email, password, first_name, last_name, gender, account_status, registration_date_and_time FROM customer WHERE customer_id = ? LIMIT 0,1";
+            $query = "SELECT customer_id, username, email, first_name, last_name, gender, account_status, image FROM customer WHERE customer_id = ? LIMIT 0,1";
             $stmt = $con->prepare($query);
 
             $stmt->bindParam(1, $customer_id);
-
             $stmt->execute();
-
             $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
             $customer_id = $row['customer_id'];
             $username = $row['username'];
             $email = $row['email'];
-            $password = $row['password'];
-            $new_password = $row['password'];
-            $confirm_password = $row['password'];
             $first_name = $row['first_name'];
             $last_name = $row['last_name'];
             $gender = $row['gender'];
             $account_status = $row['account_status'];
+            $image = $row['image'];
         } catch (PDOException $exception) {
             die('ERROR: ' . $exception->getMessage());
         }
@@ -75,27 +71,30 @@
                 $query = "UPDATE customer
                           SET username = :username, email = :email, first_name = :first_name, last_name = :last_name, gender = :gender, account_status = :account_status";
 
-                // Only update the password if it's not empty
-                if (!empty($_POST['new_password']) && !empty($_POST['confirm_password']) && !empty($_POST['old_password'])) {
+                // If a new password is provided, update the password field
+                if (!empty($_POST['new_password'])) {
                     $query .= ", password = :password";
+                    $password = md5($_POST['new_password']);
+                }
+
+                if (!empty($_FILES['image']['name'])) {
+                    $query .= ", image = :image";
+                    $image = $_FILES['image']['name'];
+                    $image_temp = $_FILES['image']['tmp_name'];
                 }
 
                 $query .= " WHERE customer_id = :customer_id";
-
                 $stmt = $con->prepare($query);
 
-                $username = strip_tags($_POST['username']); // Update the username field
-
-                // Updated code to correctly retrieve the email from the form
+                $username = strip_tags($_POST['username']);
                 $email = strip_tags($_POST['email']);
-
-                $password = strip_tags($_POST['old_password']);
-                $new_password = strip_tags($_POST['new_password']);
-                $confirm_password = strip_tags($_POST['confirm_password']);
                 $first_name = strip_tags($_POST['first_name']);
                 $last_name = strip_tags($_POST['last_name']);
                 $gender = strip_tags($_POST['gender']);
                 $account_status = strip_tags($_POST['account_status']);
+                // Handle image upload
+                $image = $_FILES['image']['name'];
+                $image_temp = $_FILES['image']['tmp_name'];
 
                 $flag = true;
 
@@ -132,19 +131,21 @@
                     $flag = false;
                 }
 
-                if (!empty($new_password) || !empty($confirm_password) || !empty($password)) {
-                    if (empty($password) || $password !== $password) {
+                // Check if a new password is provided
+                if (!empty($_POST['new_password'])) {
+                    // Check the old password
+                    if (empty($_POST['old_password']) || md5($_POST['old_password']) !== $password) {
                         echo "<div class='alert alert-danger'>Incorrect old password.</div>";
                         $flag = false;
-                    } elseif (empty($new_password) || empty($confirm_password)) {
+                    } elseif (empty($_POST['confirm_password'])) {
                         echo "<div class='alert alert-danger'>Please enter both new password and confirm password.</div>";
                         $flag = false;
-                    } elseif ($new_password !== $confirm_password) {
+                    } elseif ($_POST['new_password'] !== $_POST['confirm_password']) {
                         echo "<div class='alert alert-danger'>New password and confirm password do not match.</div>";
                         $flag = false;
                     } else {
                         // Hash the new password with MD5
-                        $password = md5($new_password);
+                        $password = md5($_POST['new_password']);
                     }
                 }
 
@@ -152,15 +153,38 @@
                     // Bind parameters
                     $stmt->bindParam(':customer_id', $customer_id);
                     $stmt->bindParam(':username', $username);
-                    $stmt->bindParam(':email', $email); // Bind the email parameter
+                    $stmt->bindParam(':email', $email);
                     $stmt->bindParam(':first_name', $first_name);
                     $stmt->bindParam(':last_name', $last_name);
                     $stmt->bindParam(':gender', $gender);
                     $stmt->bindParam(':account_status', $account_status);
-
-                    // Only bind password parameter if it's not empty
-                    if (!empty($_POST['new_password']) && !empty($_POST['confirm_password']) && !empty($_POST['old_password'])) {
+                    
+                    // If a new password is provided, bind it
+                    if (!empty($_POST['new_password'])) {
                         $stmt->bindParam(':password', $password);
+                    }
+
+                    // If an image is uploaded, move it to the target directory and bind the image parameter
+                    if (!empty($_FILES['image']['name'])) {
+                        // Define the target directory where you want to store the uploaded images
+                        $targetDir = "image/";
+
+                        // Get the original file name
+                        $originalFileName = $_FILES['image']['name'];
+
+                        // Generate a unique name for the uploaded file to avoid overwriting
+                        $uniqueFileName = time() . '_' . $originalFileName;
+
+                        // Define the path where the image will be saved
+                        $targetPath = $targetDir . $uniqueFileName;
+
+                        // Move the uploaded file to the target directory
+                        if (move_uploaded_file($_FILES['image']['tmp_name'], $targetPath)) {
+                            echo "<div class='alert alert-success'>Image uploaded successfully.</div>";
+                            $stmt->bindParam(':image', $uniqueFileName);
+                        } else {
+                            echo "<div class='alert alert-danger'>Failed to upload image.</div>";
+                        }
                     }
 
                     // Execute the query
@@ -179,7 +203,7 @@
         <div class="row">
             <div class="col-md-12">
                 <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . '?' . $_SERVER['QUERY_STRING']); ?>"
-                    method="post">
+                    method="post" enctype="multipart/form-data">
                     <div class="table-responsive">
                         <table class='table table-hover table-responsive table-bordered'>
                             <tr>
@@ -250,6 +274,10 @@
                                         <?php echo (isset($account_status) && $account_status === "pending") ? "checked" : ""; ?>>
                                     <label for="pending">Pending</label>
                                 </td>
+                            </tr>
+                            <tr>
+                                <td>Photo</td>
+                                <td><input type="file" name="image" /></td>
                             </tr>
                             <tr>
                                 <td colspan="2" class="text-center">
--- project/product_update.php
+++ project/product_update.php
@@ -1,3 +1,7 @@
+<?php
+session_start();
+?>
+
 <!DOCTYPE HTML>
 <html>
 
@@ -41,7 +45,7 @@
         // read current record's data
         try {
             // prepare select query
-            $query = "SELECT p.id, p.name, p.description, p.categoryID, c.category_name, p.price, p.promotion_price, p.manufacture_date, p.expire_date
+            $query = "SELECT p.id, p.name, p.description, p.categoryID, c.category_name, p.price, p.promotion_price, p.image, p.manufacture_date, p.expire_date
             FROM products p
             INNER JOIN product_category c ON p.categoryID = c.categoryID
             WHERE p.id = ?";
@@ -63,6 +67,7 @@
             $category_name = $row['category_name'];
             $price = $row['price'];
             $promotion_price = $row['promotion_price'];
+            $image = $row['image'];
             $manufacture_date = $row['manufacture_date'];
             $expire_date = $row['expire_date'];
         } catch (PDOException $exception) {
@@ -82,6 +87,12 @@
                 $manufacture_date = strip_tags($_POST['manufacture_date']);
                 $expire_date = strip_tags($_POST['expire_date']);
                 $categoryID = $_POST['category']; // Get categoryID from the form
+
+                // Handle image upload
+                $image = $_FILES['image']['name'];
+                $image_temp = $_FILES['image']['tmp_name'];
+
+               
 
                 // Execute the query
                 $flag = true;
@@ -114,9 +125,6 @@
                 } else {
                     $promotion_price = 0;
                 }
-                
-                // Add this code before using $category
-                $category = isset($_POST['category']) ? $_POST['category'] : $categoryID; // Use $categoryID as a fallback value
 
                 if (empty($manufacture_date)) {
                     echo "<div class='alert alert-danger'>Please select a manufacture date</div>";
@@ -127,16 +135,30 @@
                 }
 
                 if (empty($expire_date)) {
-                    echo "<div class='alert alert-danger'>Please select a manufacture date</div>";
+                    echo "<div class='alert alert-danger'>Please select an expire date</div>";
                     $flag = false;
                 } elseif ($expire_date < $manufacture_date) {
                     echo "<div class='alert alert-danger'>Expire date must be later than the manufacture date</div>";
                     $flag = false;
                 }
 
+                 // Check if a new image is uploaded
+                 if (!empty($image)) {
+                    // Check if there is an old image to delete
+                    if (!empty($row['image']) && file_exists("image/" . $row['image'])) {
+                        unlink("image/" . $row['image']);
+                    }
+
+                    // Move the new image to the destination folder
+                    move_uploaded_file($image_temp, "image/$image");
+                } else {
+                    // If no new image is uploaded, retain the old image filename
+                    $image = $row['image'];
+                }
+
                 if ($flag) {
                     // Update query
-                    $query = "UPDATE products SET name=:name, description=:description, price=:price, promotion_price=:promotion_price, manufacture_date=:manufacture_date, expire_date=:expire_date, categoryID=:categoryID WHERE id = :id";
+                    $query = "UPDATE products SET name=:name, description=:description, price=:price, promotion_price=:promotion_price, image=:image, manufacture_date=:manufacture_date, expire_date=:expire_date, categoryID=:categoryID WHERE id = :id";
 
                     // Prepare query for execution
                     $stmt = $con->prepare($query);
@@ -145,7 +167,8 @@
                     $stmt->bindParam(':name', $name);
                     $stmt->bindParam(':description', $description);
                     $stmt->bindParam(':price', $price);
-                    $stmt->bindParam(':promotion_price', $promotion_price, PDO::PARAM_STR); // Use PDO::PARAM_STR to handle null values
+                    $stmt->bindParam(':promotion_price', $promotion_price, PDO::PARAM_STR);
+                    $stmt->bindParam(':image', $image);
                     $stmt->bindParam(':manufacture_date', $manufacture_date);
                     $stmt->bindParam(':expire_date', $expire_date);
                     $stmt->bindParam(':categoryID', $categoryID);
@@ -165,16 +188,19 @@
         ?>
 
         <!-- Display the existing data and allow updates -->
-        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="post">
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"] . "?id={$id}"); ?>" method="post"
+            enctype="multipart/form-data">
             <table class='table table-hover table-responsive table-bordered'>
                 <tr>
                     <td>Name</td>
-                    <td><input type='text' name='name' value="<?php echo htmlspecialchars($name, ENT_QUOTES); ?>" class='form-control' />
-                    </td>
+                    <td><input type='text' name='name' value="<?php echo htmlspecialchars($name, ENT_QUOTES); ?>"
+                            class='form-control' /></td>
                 </tr>
                 <tr>
                     <td>Description</td>
-                    <td><textarea name='description' class='form-control'><?php echo htmlspecialchars($description, ENT_QUOTES); ?></textarea></td>
+                    <td><textarea name='description'
+                            class='form-control'><?php echo htmlspecialchars($description, ENT_QUOTES); ?></textarea>
+                    </td>
                 </tr>
                 <tr>
                     <td>Category</td>
@@ -199,38 +225,39 @@
                 </tr>
                 <tr>
                     <td>Price</td>
-                    <td colspan="2">
-                        <input type='text' name='price' value="<?php echo htmlspecialchars($price, ENT_QUOTES); ?>" class='form-control' />
-                    </td>
+                    <td colspan="2"><input type='text' name='price'
+                            value="<?php echo htmlspecialchars($price, ENT_QUOTES); ?>" class='form-control' /></td>
                 </tr>
                 <tr>
                     <td>Promotion Price</td>
-                    <td colspan="2">
-                        <input type='text' name='promotion_price' value="<?php echo htmlspecialchars($promotion_price, ENT_QUOTES); ?>" class='form-control' />
+                    <td colspan="2"><input type='text' name='promotion_price'
+                            value="<?php echo htmlspecialchars($promotion_price, ENT_QUOTES); ?>"
+                            class='form-control' /></td>
+                </tr>
+                <tr>
+                    <td>Photo</td>
+                    <td><input type="file" name="image" />
                     </td>
                 </tr>
                 <tr>
                     <td>Manufacture Date</td>
-                    <td colspan="2">
-                        <input type='date' name='manufacture_date' class='form-control' value="<?php echo isset($manufacture_date) ? $manufacture_date : ''; ?>">
+                    <td colspan="2"><input type='date' name='manufacture_date'
+                            class='form-control' value="<?php echo isset($manufacture_date) ? $manufacture_date : ''; ?>">
                     </td>
                 </tr>
                 <tr>
-                    <td>Expired Date</td>
-                    <td colspan="2">
-                        <input type='date' name='expire_date' class='form-control' value="<?php echo isset($expire_date) ? $expire_date : ''; ?>">
+                    <td>Expire Date</td>
+                    <td colspan="2"><input type='date' name='expire_date'
+                            class='form-control' value="<?php echo isset($expire_date) ? $expire_date : ''; ?>">
                     </td>
                 </tr>
                 <tr>
                     <td></td>
-                    <td colspan="2">
-                        <input type='submit' value='Save Changes' class='btn btn-primary' />
-                        <a href='product_index.php' class='btn btn-danger'>Back to read products</a>
-                    </td>
+                    <td colspan="2"><input type='submit' value='Save Changes' class='btn btn-primary' /> <a
+                            href='product_index.php' class='btn btn-danger'>Back to read products</a></td>
                 </tr>
             </table>
         </form>
-
     </div>
     <!-- end .container -->
 </body>
